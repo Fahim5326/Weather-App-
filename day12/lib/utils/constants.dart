ce37fb2576f571b2c22cedf43e9b6f2f72da46b7
Fahim <fahimfeni149@gmail.com>
2023-11-23 09:04:19 +0600

    Final Version

---
+++ day12/lib/utils/constants.dart
@@ -0,0 +1,28 @@
+const weatherApiKey = '8012a2a16e8851dfba540307bd03cca6';
+const degree = '\u00B0';
+const metric = 'metric';
+const imperial = 'imperial';
+const celsius = 'C';
+const fahrenheit = 'F';
+
+const iconUrlPrefix = 'https://openweathermap.org/img/wn/';
+const iconUrlSuffix = '@2x.png';
+
+const backgroundImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnirDwTrEQ1o_6cH93_fC4wVqJrfUBQwob_MM3fUpnlUuBjCS33FnD_lRMzzM5Oy5GI74&usqp=CAU';
+
+const cities = [
+  'Barisal',
+  'Chattagram',
+  'Chandpur',
+  'Comilla',
+  'Dhaka',
+  'Feni',
+  'Foridpur',
+  'Gazipur',
+  'Hobigonj',
+  'Hajiganj',
+  'Khulna',
+  'Noakhali',
+  'Rangpur',
+  'Rajshahi'
+];
